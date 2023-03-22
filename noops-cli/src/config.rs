e413a5ff5470f8cd1b46a0c6ea834dfa20b066db
falumpaset <marc@jfcomputing.io>
2023-03-22 17:40:17 +0100

    style: format

--- noops-cli/src/config.rs
+++ noops-cli/src/config.rs
@@ -6,13 +6,10 @@ use serde::{Deserialize, Serialize};
 use crate::{modules::Module, print};
 
 pub fn init() -> Config {
-    let config_name = print::Color::prompt_text(
-        &print::Color::White,
-        "Name your Project",
-    );
+    let config_name = print::Color::prompt_text(&print::Color::White, "Name your Project");
     let config = Config::new(&config_name);
     config.to_yaml(None).unwrap();
-    return config
+    config
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
@@ -80,30 +77,27 @@ mod tests {
     use crate::{config::Config, modules::Module};
     const TEST_CONFIG_PATH: &str = "test/noops-config.yaml";
 
-
     #[test]
     fn test_from_yaml() {
-        
         let parsed_config = Config::from_yaml(TEST_CONFIG_PATH).unwrap();
-    
+
         let mut wanted_config = Config::new("noops-example");
         let example_module = Module::new("my-module", "test/", "my super duper module", "dummy");
         wanted_config.add_module(example_module).unwrap();
-    
+
         assert_eq!(parsed_config, wanted_config);
     }
-    
+
     #[test]
     fn test_to_yaml() {
         let config = Config::from_yaml(TEST_CONFIG_PATH).unwrap();
         let saved_config_path = "test/saved.yaml";
 
         config.to_yaml(Some(saved_config_path)).unwrap();
-    
+
         let written_config = Config::from_yaml(saved_config_path).unwrap();
         assert_eq!(config, written_config);
-    
+
         crate::helpers::filesystem::delete_file(saved_config_path)
-    
     }
 }
