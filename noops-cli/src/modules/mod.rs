6990fa3f3bd9c8f41e5b9dab4a1f1d86f71ae984
falumpaset <marc@jfcomputing.io>
2023-03-23 01:58:04 +0100

    chore: add language to module and template

--- noops-cli/src/modules/mod.rs
+++ noops-cli/src/modules/mod.rs
@@ -4,11 +4,27 @@ use self::templates::ModuleTemplate;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
+enum Language {
+    Rust,
+    Golang,
+}
+
+impl Language {
+    fn to_string(&self) -> String {
+        match self {
+            Language::Rust => String::from("Rust"),
+            Language::Golang => String::from("Golang"),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 pub struct Module {
     pub name: String,
     pub root: std::path::PathBuf,
     pub template: String,
     description: String,
+    language: Language,
 }
 
 impl Module {
@@ -17,12 +33,14 @@ impl Module {
         root: impl AsRef<std::path::Path>,
         description: &str,
         template: &str,
+        language: Language,
     ) -> Self {
         Self {
             name: name.to_string(),
             root: root.as_ref().to_path_buf(),
             description: description.to_string(),
             template: template.to_string(),
+            language,
         }
     }
     pub fn to_vec_string(&self) -> Vec<String> {
@@ -31,6 +49,7 @@ impl Module {
             self.name.clone(),
             self.template.clone(),
             self.description.clone(),
+            self.language.to_string(),
         ]
     }
 }
@@ -42,6 +61,7 @@ impl From<ModuleTemplate> for Module {
                 name: _,
                 description,
                 repository,
+                language,
                 module_name,
                 module_root,
             } => {
@@ -53,6 +73,7 @@ impl From<ModuleTemplate> for Module {
                         .unwrap_or_else(|| std::path::PathBuf::from(module_name + "/")),
                     description: description.to_string(),
                     template: repository.to_string(),
+                    language,
                 }
             }
         }
