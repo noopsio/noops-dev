7b76d3897b2c9a0ac2e8d3258322c214f43bb3a5
Guillaume Fournier <35076723+Giom-fm@users.noreply.github.com>
2023-07-23 16:01:03 +0200

    feat: Consolidate cli commands into subcommands (#128)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 mod adapter;
 mod client;
+mod commands;
 mod config;
 mod filesystem;
 mod handlers;
@@ -10,7 +11,7 @@ mod terminal;
 use std::{env, fs, path::Path};
 
 use adapter::git::GitAdapter;
-use clap::{command, Command};
+use clap::Parser;
 use client::NoopsClient;
 use config::Config;
 use reqwest::Url;
@@ -21,78 +22,65 @@ const CONFIG_NAME: &str = "noops";
 
 fn main() -> anyhow::Result<()> {
     env_logger::init();
-    let terminal = Terminal::new();
-    let mut commands = create_commands();
 
+    let terminal = Terminal::new();
     let xdg_config_home = env::var("XDG_CONFIG_HOME")?;
     let config_path = Path::new(&xdg_config_home).join(CONFIG_NAME);
     fs::create_dir_all(config_path.clone())?;
 
-    match commands.clone().get_matches().subcommand() {
-        Some(("init", _)) => {
-            handlers::project::init(&terminal)?;
-        }
+    let cli = commands::Cli::parse();
 
-        Some(("build", _)) => {
-            let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
-            handlers::project::build(&terminal, &config)?;
-        }
-
-        Some(("deploy", _)) => {
-            let base_url = Url::parse(BASE_URL)?;
-            let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
-            let jwt = handlers::auth::get_jwt(&config_path)?;
-            if jwt.is_none() {
-                terminal.write_text("You are not logged in.")?;
-                return Ok(());
-            }
-            let client = NoopsClient::new(base_url, config.project_name.clone(), jwt);
-            handlers::project::deploy(&terminal, &config, &client)?;
-        }
-
-        Some(("destroy", _)) => {
-            let base_url = Url::parse(BASE_URL)?;
-            let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
-            let jwt = handlers::auth::get_jwt(&config_path)?;
-            if jwt.is_none() {
-                terminal.write_text("You are not logged in.")?;
-                return Ok(());
-            }
-            let client = NoopsClient::new(base_url, config.project_name.clone(), jwt);
-            handlers::project::destroy(&terminal, &client, &config.project_name)?;
-        }
-
-        Some(("add", _)) => {
-            let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
-            let git = GitAdapter::new();
-            handlers::modules::add(&terminal, config, &git)?;
-        }
-
-        Some(("remove", _)) => {
-            let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
-            handlers::modules::delete(&terminal, config)?;
-        }
-
-        Some(("login", _)) => {
+    match &cli.commands {
+        commands::Commands::Login => {
             let base_url = Url::parse(BASE_URL)?;
             let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
             let client = NoopsClient::new(base_url, config.project_name, None);
             handlers::auth::login(&client, &terminal, &config_path)?;
         }
-
-        _ => commands.print_help()?,
+        commands::Commands::Project(project_subcommand) => match project_subcommand {
+            commands::ProjectCommands::Create => handlers::project::init(&terminal)?,
+            commands::ProjectCommands::Build => {
+                let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
+                handlers::project::build(&terminal, &config)?;
+            }
+            commands::ProjectCommands::Deploy => {
+                let base_url = Url::parse(BASE_URL)?;
+                let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
+                let jwt = handlers::auth::get_jwt(&config_path)?;
+                if jwt.is_none() {
+                    terminal.write_text("You are not logged in.")?;
+                    return Ok(());
+                }
+                let client = NoopsClient::new(base_url, config.project_name.clone(), jwt);
+                handlers::project::deploy(&terminal, &config, &client)?;
+            }
+            commands::ProjectCommands::Destroy => {
+                let base_url = Url::parse(BASE_URL)?;
+                let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
+                let jwt = handlers::auth::get_jwt(&config_path)?;
+                if jwt.is_none() {
+                    terminal.write_text("You are not logged in.")?;
+                    return Ok(());
+                }
+                let client = NoopsClient::new(base_url, config.project_name.clone(), jwt);
+                handlers::project::destroy(&terminal, &client, &config.project_name)?;
+            }
+            commands::ProjectCommands::Show => todo!(),
+        },
+        commands::Commands::Function(function_subcommand) => match function_subcommand {
+            commands::FunctionCommands::Create => {
+                let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
+                let git = GitAdapter::new();
+                handlers::modules::add(&terminal, config, &git)?;
+            }
+            commands::FunctionCommands::Build => todo!(),
+            commands::FunctionCommands::Deploy => todo!(),
+            commands::FunctionCommands::Destroy => {
+                let config = Config::from_yaml(config::CONFIG_FILE_NAME)?;
+                handlers::modules::delete(&terminal, config)?;
+            }
+            commands::FunctionCommands::Show => todo!(),
+        },
     }
-
     Ok(())
-}
-
-fn create_commands() -> Command {
-    command!()
-        .subcommand(Command::new("login").about("Login in the noops cloud"))
-        .subcommand(Command::new("init").about("Create a new project"))
-        .subcommand(Command::new("add").about("Add a new module"))
-        .subcommand(Command::new("remove").about("Remove a module"))
-        .subcommand(Command::new("build").about("Builds all functions in project"))
-        .subcommand(Command::new("deploy").about("Deploy the project"))
-        .subcommand(Command::new("destroy").about("Destroy the project"))
 }
